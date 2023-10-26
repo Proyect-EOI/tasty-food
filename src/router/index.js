1aa7f36b74634a6e41c1634288d73b71e649f89e
gonzsanz <davidgonzsanz02@gmail.com>
2023-10-26 20:11:00 +0200

    intento de arreglo de routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import NotFoundView from '../views/NotFoundView.vue'
 import AboutUs from '../views/AboutUs.vue'
+import DetailsView from '../views/DetailsView.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,8 +19,8 @@ const router = createRouter({
         {
             path: '/recipes/:id',
             name: 'recipes',
-            component: () => import('../views/DetailsView.vue'),
-
+            component: DetailsView,
+            props: true
         },
         {
             path: '/results',
